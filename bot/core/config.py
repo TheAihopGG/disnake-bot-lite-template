1a0deaa6d53e0874f67ff1595b65c0756e58552a
TheAihopGG <theaihopgg@gmail.com>
2026-02-07 16:32:42 +0700

    Добавлена конфигурация бота

---
+++ bot/core/config.py
@@ -0,0 +1,24 @@
+from typing import Literal
+from logging import DEBUG
+from pathlib import Path
+
+from core.utils import get_env
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+BOT_TOKEN: str = get_env("BOT_TOKEN", raise_on_none=True)
+
+POSTGRES_PASSWORD: str = get_env("POSTGRES_PASSWORD", raise_on_none=True)
+POSTGRES_USERNAME: str = get_env("POSTGRES_USERNAME", raise_on_none=True)
+POSTGRES_DB: str = get_env("POSTGRES_DB", raise_on_none=True)
+
+LOGGING_FILENAME = BASE_DIR / "logs.log"
+LOGGING_LEVEL: Literal[10, 20, 30, 40, 50] = DEBUG
+
+__all__ = (
+    "BOT_TOKEN",
+    "POSTGRES_PASSWORD",
+    "POSTGRES_USERNAME",
+    "POSTGRES_DB",
+)
