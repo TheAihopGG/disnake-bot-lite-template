a38395aa98a1778d29ad0b81901c7b01164fe726
TheAihopGG <theaihopgg@gmail.com>
2026-02-08 14:50:45 +0700

    Исправлен скрипт для выполнения миграций

--- bot/prestart.sh
+++ bot/prestart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 echo Applying migrations
-yoyo apply --database postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB --batch
+yoyo apply --database postgresql+psycopg://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB --batch
 echo Migrations applied
 exec "$@"
